3f184a89c8ebd283898c34672549944e1cf250a2
AnkurDev-io <duttarik894@gmail.com>
2026-01-28 09:46:48 +0530

    Add files via upload
    (05/01/2026) Lab assignment

---
+++ main.py
@@ -0,0 +1,65 @@
+#First Program
+# Name = input("Enter Name")
+# print("My name is ",Name)
+
+# a=int(input("Enter the value of a:"))
+# b=int(input("Enter the value of b:"))
+# c=a+b
+# print("The result is",c)
+
+# p = int(input("Enter the value of p:"))
+# t= int(input("Enter the value of t:"))
+# r=int(input("Enter the value of r:"))
+
+# simpleInterest = (p*t*r)/100
+
+# print("The value is",simpleInterest)
+
+# a=10
+# b=20
+# c=b//a
+
+# print(c)
+
+#square of a number
+# a=int(input("Enter the value of a:"))
+# squareValue = a**3
+# print("The value is:",squareValue)
+
+#Write a code to calculate x to the power n
+# x = int(input("Enter the value of x:"))
+# n = int(input("Enter the value of n:"))
+
+# val = x**n
+# print("The value is ", val)
+
+# writw a python code to find out the square root of a number
+# x=int(input("Enter the value of x:"))
+# squareRoot = x**0.5
+# print("The value is:",int(squareRoot))
+
+#Area of Triangle
+#case1
+
+# base = int(input("Enter the value of base:"))
+# height=int(input("Enter the value of height:"))
+# value = 0.5*base*height
+# print("Area of Triangle :",value)
+#case2
+# import math
+# firstSide =int(input("Enter the value :"))
+# secondSide = int(input("Enter the value :"))
+# thirdSide = int(input("Enter the value:"))
+# s=(firstSide+secondSide+thirdSide)/2
+# value = math.sqrt(s*((s-firstSide)*(s-secondSide)*(s-thirdSide)))
+# print("Value is:",int(value))
+
+#Area and perimetre od a circle
+
+PI = 3.14
+rad = int(input("Enter the value of rad:"))
+area = PI*(rad**2)
+perimetre = 2*PI*(rad**2)
+
+print("Area is :", area)
+print("Perimetre is :", perimetre)
